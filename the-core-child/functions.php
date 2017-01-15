9b3ac26676b74097cc848a724e8f032f0b5add7d
Alexandr <nita.alexandr@gmail.com>
2017-01-16 00:56:09 +0200

    Add styles for the menu and logo

--- the-core-child/functions.php
+++ the-core-child/functions.php
@@ -3,7 +3,7 @@
 }
 
 function the_core_theme_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'parent-style', get_stylesheet_directory_uri() . '/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'the_core_theme_enqueue_styles' );
 
